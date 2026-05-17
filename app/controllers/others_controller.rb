44178acbc6244e96459f216b257244ef33745032
rena <wpjgjglp@gmail.com>
2026-05-17 23:02:22 +0900

    OthersControllerのクラス定義を修正

--- app/controllers/others_controller.rb
+++ app/controllers/others_controller.rb
@@ -1,26 +1,34 @@
-def create
-  if params[:vegetable_name].blank?
-    flash.now[:alert] = "野菜名を入力してください"
+class OthersController < ApplicationController
+  before_action :require_login
+
+  def new
     @stock = Stock.new
-    render :new, status: :unprocessable_entity
-    return
   end
 
-  vegetable = Vegetable.find_or_create_by(name: params[:vegetable_name]) do |v|
-    v.shelf_life_days = 5
-  end
+  def create
+    if params[:vegetable_name].blank?
+      flash.now[:alert] = "野菜名を入力してください"
+      @stock = Stock.new
+      render :new, status: :unprocessable_entity
+      return
+    end
 
-  purchased_on = params[:purchased_on].present? ? params[:purchased_on] : Date.today
+    vegetable = Vegetable.find_or_create_by(name: params[:vegetable_name]) do |v|
+      v.shelf_life_days = 5
+    end
 
-  @stock = current_user.stocks.build(
-    vegetable: vegetable,
-    purchased_on: purchased_on,
-    status: :active
-  )
+    purchased_on = params[:purchased_on].present? ? params[:purchased_on] : Date.today
 
-  if @stock.save
-    redirect_to stocks_path, notice: "野菜を登録しました！"
-  else
-    render :new, status: :unprocessable_entity
+    @stock = current_user.stocks.build(
+      vegetable: vegetable,
+      purchased_on: purchased_on,
+      status: :active
+    )
+
+    if @stock.save
+      redirect_to stocks_path, notice: "野菜を登録しました！"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
