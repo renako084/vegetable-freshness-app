4f69c258b73c0c9a8ec811b2abddffeddb4f07e3
rena <wpjgjglp@gmail.com>
2026-05-17 23:33:11 +0900

    バリデーションメッセージを日本語に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :stocks, dependent: :destroy
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: { message: "を入力してください" },
+                    uniqueness: { message: "はすでに登録されています" }
 end
