e8e3544d27cc238f33b4f9c358f81e60dda96350
rena <wpjgjglp@gmail.com>
2026-05-18 21:42:11 +0900

    トップページの文言を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "アカウントを作成しました！"
+      redirect_to stocks_path, notice: "アカウントを作成しました！"
     else
       render :new, status: :unprocessable_entity
     end
