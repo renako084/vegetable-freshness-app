a33505dcefc11fcde433d253e6302cb9523ada65
rena <wpjgjglp@gmail.com>
2026-05-19 12:21:52 +0900

    パスワードの最低文字数バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :stocks, dependent: :destroy
   validates :email, presence: { message: "を入力してください" },
                     uniqueness: { message: "はすでに登録されています" }
+  validates :password, length: { minimum: 8, message: "は8文字以上で入力してください" }, if: -> { password.present? }
 end
